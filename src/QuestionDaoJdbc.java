334f7ee617364840e668e625de962a186201fcf7
dmstjr930310 <dmstjr930310@naver.com>
2020-07-16 18:08:27 +0900

    퀘션다오제이디비씨 맨 밑에 퍼블릭 셀렉트 부분 추가

--- src/QuestionDaoJdbc.java
+++ src/QuestionDaoJdbc.java
@@ -1,5 +1,4 @@
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -21,62 +20,57 @@ public class QuestionDaoJdbc {
 	String user = "project";
 	String password = "project01";
 
-	
-	//모든 단어, 설명을 가져오는 임시용 메소드
+	// 모든 단어, 설명을 가져오는 임시용 메소드
 	public ArrayList<QuestionVo> selectQuestionList() {
 		ArrayList<QuestionVo> questionList = new ArrayList<QuestionVo>();
 		String sql = "SELECT question_word, question_explain FROM question ORDER BY question_reg_date DESC";
-		
-		try (
-				Connection conn = DriverManager.getConnection(url, user, password);
+
+		try (Connection conn = DriverManager.getConnection(url, user, password);
 				PreparedStatement pstmt = conn.prepareStatement(sql);
-				ResultSet rs = pstmt.executeQuery();
-				){
-			
-			while(rs.next()) {
+				ResultSet rs = pstmt.executeQuery();) {
+
+			while (rs.next()) {
 				QuestionVo vo = new QuestionVo();
 				vo.setQuestionWord(rs.getString("question_word"));
 				vo.setQuestionExplain(rs.getString("question_explain"));
 				questionList.add(vo);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return questionList;
 	}
-	
-	
+
 	public ArrayList<QuestionVo> selectRecentQuestionList(String id) {
 		ArrayList<QuestionVo> questionList = new ArrayList<QuestionVo>();
-		String sql = "SELECT question_word FROM question WHERE question_writer = '" + id + "' ORDER BY question_reg_date DESC";
-		
-		try (
-				Connection conn = DriverManager.getConnection(url, user, password);
+		String sql = "SELECT question_word FROM question WHERE question_writer = '" + id
+				+ "' ORDER BY question_reg_date DESC";
+
+		try (Connection conn = DriverManager.getConnection(url, user, password);
 				PreparedStatement pstmt = conn.prepareStatement(sql);
-				ResultSet rs = pstmt.executeQuery();
-				){
-			
+				ResultSet rs = pstmt.executeQuery();) {
+
 //			pstmt.setString(1, id);
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				QuestionVo vo = new QuestionVo();
 				vo.setQuestionWord(rs.getString("question_word"));
 				questionList.add(vo);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return questionList;
 	}
-	
-	
+
 	public int insertQustion(QuestionVo vo) {
 
 		int num = 0;
 
-		String sql = "INSERT INTO question (question_no, question_word, question_explain, question_writer) " + "VALUES (seq_question_no.NEXTVAL, ?, ?, ?)";
+		String sql = "INSERT INTO question (question_no, question_word, question_explain, question_writer) "
+				+ "VALUES (seq_question_no.NEXTVAL, ?, ?, ?)";
 
 		try (Connection conn = DriverManager.getConnection(url, user, password);
 				PreparedStatement pstmt = conn.prepareStatement(sql);) {
@@ -90,8 +84,33 @@ public class QuestionDaoJdbc {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return num;
 	}
 
+	public QuestionVo extractQuestion(QuestionVo questionvo) {
+		QuestionVo vo = null;
+		String sql = "SELECT question_word, question_explain FROM question WHERE question_word = ? and question_explain = ?";
+
+		try (Connection conn = DriverManager.getConnection(url, user, password);
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+			pstmt.setString(1, vo.getQuestionWord());
+			pstmt.setString(2, vo.getQuestionExplain());
+
+			try (ResultSet rs = pstmt.executeQuery();) {
+				if (rs.next()) {
+
+					vo = new QuestionVo();
+
+					vo.setQuestionWord(rs.getString("question_word"));
+					vo.setQuestionExplain(rs.getString("question_explain"));
+					//vo.setMemNickname(rs.getString("mem_nickname"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return vo;
+	}
 }
