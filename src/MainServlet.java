cb0e7f627d53765e39c6073a34b19440f325c946
dmstjr930310 <dmstjr930310@naver.com>
2020-07-21 17:36:51 +0900

    기존 문제 내는 곳 있었던 곳에 랜덤으로 문제 뽑기, 똑같은 문제 나오는 것은 주석 처리

--- src/MainServlet.java
+++ src/MainServlet.java
@@ -18,9 +18,47 @@ public class MainServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		int num = questionDao.maxQuestionNumber();
+
+		int no = (int) (Math.random() * num + 1);
+		QuestionVo questionVo = questionDao.extractQuestion(no);
+//		boolean isDuplicate = false;
+
+//		for (int i = 0; i < numArr.size(); i++) {
+//			if (no == numArr.get(i)) {
+//				isDuplicate = true;
+//			}
+//		}
+		
+		if (questionVo == null /*|| isDuplicate*/) {
+			boolean isEmpty = true;
+
+			while (isEmpty /*|| isDuplicate*/) {
+
+				no = (int) (Math.random() * num + 1);
+				questionVo = questionDao.extractQuestion(no);
+
+				isEmpty = false;
+//				isDuplicate = false;
+
+				if (questionVo == null) {
+					isEmpty = true;
+				}
+
+//				for (int i = 0; i < numArr.size(); i++) {
+//					if (no == numArr.get(i)) {
+//						isDuplicate = true;
+//					}
+//				}
+			}
+			
+		}
+
+//		numArr.add(no);
+
+		req.setAttribute("questionVo", questionVo);
+
 		req.getRequestDispatcher("/WEB-INF/jsp/project/main.jsp").forward(req, resp);
-		
-		//int n = (int) Math.random() * 99 + 1;
-		
 	}
 }
+
