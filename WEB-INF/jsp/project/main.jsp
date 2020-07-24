952884ff1894cb827747b6482fa92c64eae4e8ac
dmstjr930310 <dmstjr930310@naver.com>
2020-07-24 14:57:33 +0900

    정답, 오답, 전체, 문제 풀어도 누적 되게끔 수정.

--- WEB-INF/jsp/project/main.jsp
+++ WEB-INF/jsp/project/main.jsp
@@ -35,36 +35,27 @@
     <div class="container">
         <center>
             <div id="wrap">
+			<form action="${pageContext.request.contextPath}/main.do" method=post>
                 <p id=q>- 문제 : ${questionVo.questionExplain}</p>
-                <input id='my' placeholder="답을 입력하세요"/>
-                <br><br><br><br><br>
-                <input type="hidden" id='ans' value='${questionVo.questionWord}' />
-                <button id='aaa'>다음문제</button>
+                <input name="myAns" placeholder="답을 입력하세요"/>
                 
-            </div>
+                <br><br><br><br><br>
+               
+                <button id='aaa' type="submit">다음문제</button>
+                
+                <br /><br />
+
+			        	정답
+			    	<input style="font-size: 20px;" id='correct' value="${countVo.corCount}" disabled/>
+			    	<br />
+			        	오답
+			    	<input style="font-size: 20px;" id='error' value="${countVo.errCount}" disabled/>
+			    	<br />
+			        	전체
+			    	<input style="font-size: 20px;" id='total' value="${countVo.totCount}" disabled/>
+			</form>  
+			</div>
         </center>
     </div>
-    <script>
-    	var btn = document.querySelector('#aaa');
-    	btn.onclick = function() {
-    		check();
-			location.reload();
-		}
-    	
-    	function check(){
-    		var my = document.querySelector('#my').value;
-            var ans = document.querySelector('#ans').value
-            
-            if(ans == my){
-                alert('정답');
-            }
-            else if(my == ''){
-                alert('값을 입력하세요');
-            }
-            else{
-                alert('오답');
-            }
-    	}
-    </script>
 </body>
 </html>
--- src/MainServlet.java
+++ src/MainServlet.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,58 +9,84 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/main.do")
-public class MainServlet extends HttpServlet{
-	
+public class MainServlet extends HttpServlet {
+
 	MainDaoJdbc mainDao = new MainDaoJdbc();
 	QuestionDaoJdbc questionDao = new QuestionDaoJdbc();
-	
+	ArrayList<Integer> numArr = new ArrayList<Integer>();
+	CountVo countVo = new CountVo();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		int num = questionDao.maxQuestionNumber();
 
 		int no = (int) (Math.random() * num + 1);
 		QuestionVo questionVo = questionDao.extractQuestion(no);
-//		boolean isDuplicate = false;
+		boolean isDuplicate = false;
 
-//		for (int i = 0; i < numArr.size(); i++) {
-//			if (no == numArr.get(i)) {
-//				isDuplicate = true;
-//			}
-//		}
+		for (int i = 0; i < numArr.size(); i++) {
+			if (no == numArr.get(i)) {
+				isDuplicate = true;
+			}
+		}
 		
-		if (questionVo == null /*|| isDuplicate*/) {
+		if (questionVo == null || isDuplicate) {
 			boolean isEmpty = true;
 
-			while (isEmpty /*|| isDuplicate*/) {
+			while (isEmpty || isDuplicate) {
 
 				no = (int) (Math.random() * num + 1);
 				questionVo = questionDao.extractQuestion(no);
 
-				isEmpty = false;
-//				isDuplicate = false;
+				isEmpty = false;				isDuplicate = false;
 
 				if (questionVo == null) {
 					isEmpty = true;
 				}
 
-//				for (int i = 0; i < numArr.size(); i++) {
-//					if (no == numArr.get(i)) {
-//						isDuplicate = true;
-//					}
-//				}
+				for (int i = 0; i < numArr.size(); i++) {
+					if (no == numArr.get(i)) {
+						isDuplicate = true;
+					}
+				}
 			}
 			
 		}
 
-//		numArr.add(no);
+		numArr.add(no);
 
 		req.setAttribute("questionVo", questionVo);
-
+		HttpSession session = req.getSession();
+		session.setAttribute("questionVo", questionVo);
+		
 		req.getRequestDispatcher("/WEB-INF/jsp/project/main.jsp").forward(req, resp);
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String myAns = req.getParameter("myAns");
+		HttpSession session = req.getSession();
+		
+		QuestionVo questionVo = (QuestionVo) session.getAttribute("questionVo");
+		String ans = questionVo.getQuestionWord();
+		
+		if(myAns.equals(ans)) {
+			countVo.setCorCount(countVo.getCorCount()+1);
+			countVo.setTotCount(countVo.getTotCount()+1);
+		}else {
+			countVo.setErrCount(countVo.getErrCount()+1);
+			countVo.setTotCount(countVo.getTotCount()+1);
+		}
+		
+		session.setAttribute("countVo", countVo);
+		
+		resp.sendRedirect(req.getContextPath() + "/main.do");
+	}
+	
 }
 
